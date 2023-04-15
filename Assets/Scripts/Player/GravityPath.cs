748013be0eab65e26ec1f87472aa36be13cf7843
MikkelBybjerg <mikkel.bybjerg@hotmail.com>
2023-04-15 18:33:13 +0200

    Add gravity pathing to player prefab

--- Assets/Scripts/Player/GravityPath.cs
+++ Assets/Scripts/Player/GravityPath.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GravityPath : MonoBehaviour
 {
     public GameObject PathVisualPrefab;
-    public Vector3 CurrentVelocity {get => PathMomentum;}
+    public float PathSpeed;
 
     private Transform PathVisual;
     private Vector3 PathMomentum;
@@ -17,26 +17,34 @@ public class GravityPath : MonoBehaviour
         PathVisual = Instantiate(PathVisualPrefab, transform.position, Quaternion.identity).transform;
         PathVisual.SetParent(transform);
         PathMomentum = Vector3.right * 5;
+        PathSpeed = 1;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         RenderPath(2);
+        (transform.position, PathMomentum) = PathDelta(transform.position, PathMomentum, Time.fixedDeltaTime*PathSpeed, GetAttractors());
+    }
+
+    private List<Attractor> GetAttractors()
+    {
+        List<Attractor> attractors = new List<Attractor>();
+            foreach(Attractor a in GameObject.FindObjectsOfType<Attractor>())
+                if(a.Activated)
+                    attractors.Add(a);
+        return attractors;
     }
 
     private void RenderPath(float timeAhead)
     {
-        int pointRange = 30;
+        int pointRange = 100;
         float delta = timeAhead/pointRange;
 
         PathVisual.position = transform.position;
         LineRenderer line = PathVisual.GetComponent<LineRenderer>();
         line.positionCount = pointRange;
 
-        List<Attractor> attractors = new List<Attractor>();
-        foreach(Attractor a in GameObject.FindObjectsOfType<Attractor>())
-            if(a.Activated)
-                attractors.Add(a);
+        List<Attractor> attractors = GetAttractors();
         
         Vector3 momentum = PathMomentum;
         Vector3 position = transform.position;
