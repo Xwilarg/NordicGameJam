02d200dd85df3112b18ade5c21b49c65dda3f0df
Xwilarg <xwilarg@protonmail.com>
2023-04-15 15:24:36 +0200

    Add initial player rotation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,11 +14,44 @@ namespace NordicGameJam.Player
         private int _maxForce;
         private float _timer;
 
+        // Things when we didn't move yet
+        private bool _didMove;
+        private bool _aimDirection;
+        private float _aimTimer;
+
+        private float _baseAngle;
+        private float _startAngle, _endAngle;
+
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
             _rb.drag = _info.LinearDrag;
             _timer = Time.unscaledTime;
+
+            _baseAngle = transform.rotation.eulerAngles.z;
+            _aimTimer = _info.RotationSpeed;
+
+            _startAngle = _baseAngle - _info.MinAngle;
+            _endAngle = _baseAngle + _info.MinAngle;
+        }
+
+        private void Update()
+        {
+            if (!_didMove)
+            {
+                _aimTimer -= Time.deltaTime;
+                if (_aimTimer <= 0)
+                {
+                    _aimTimer = _info.RotationSpeed;
+                    _aimDirection = !_aimDirection;
+                }
+
+                transform.rotation = Quaternion.Euler(
+                    x: transform.rotation.eulerAngles.x,
+                    y: transform.rotation.eulerAngles.y,
+                    z: Mathf.Lerp(_aimDirection ? _startAngle : _endAngle, _aimDirection ? _endAngle : _startAngle, _aimTimer / _info.RotationSpeed)
+                );
+            }
         }
 
         private void FixedUpdate()
@@ -33,9 +66,8 @@ namespace NordicGameJam.Player
         {
             if (collision.collider.CompareTag("Wall"))
             {
-                _rb.velocity = collision.relativeVelocity;
                 var angle = Mathf.Atan2(_rb.velocity.y, _rb.velocity.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90f));
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - _baseAngle - 180f));
             }
         }
 
@@ -43,14 +75,19 @@ namespace NordicGameJam.Player
         {
             if (value == 0) // Propulse player
             {
-                // Apply force to the player
-                var timeDiff = Mathf.Clamp(Time.unscaledTime - _timer, 0f, _info.MaxPressDuration);
-                _rb.AddForce(transform.up *
-                    _info.BaseSpeed *
-                    _info.PressionModifier.Evaluate(_maxForce / 100f) * // LEGO SDK always return a value between 0 and 100
-                    _info.DurationModifier.Evaluate(timeDiff / 3f) *
-                    Time.fixedDeltaTime
-                    , ForceMode2D.Impulse);
+                if (_maxForce > 0)
+                {
+                    // Apply force to the player
+                    var timeDiff = Mathf.Clamp(Time.unscaledTime - _timer, 0f, _info.MaxPressDuration);
+                    _rb.AddForce(transform.up *
+                        _info.BaseSpeed *
+                        _info.PressionModifier.Evaluate(_maxForce / 100f) * // LEGO SDK always return a value between 0 and 100
+                        _info.DurationModifier.Evaluate(timeDiff / 3f) *
+                        Time.fixedDeltaTime
+                        , ForceMode2D.Impulse);
+
+                    _didMove = true;
+                }
 
                 // Reset stuffs
                 _maxForce = 0;
