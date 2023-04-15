359a166efacb58d10f8898ffd28d406b0e784a85
MikkelBybjerg <mikkel.bybjerg@hotmail.com>
2023-04-15 23:43:17 +0200

    Add player rotation pointing

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -61,7 +61,7 @@ namespace NordicGameJam.Player
             else
             {
                 float angle = Mathf.Atan2(_path.CurrentMomentum.y, _path.CurrentMomentum.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+                _rotationTarget.rotation = Quaternion.Euler(0f, 0f, angle - 90);
             }
         }
 
