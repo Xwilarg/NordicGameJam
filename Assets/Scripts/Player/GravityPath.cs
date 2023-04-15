dcc732048d68c793988adef643b6b5dd09a94b17
MikkelBybjerg <mikkel.bybjerg@hotmail.com>
2023-04-15 23:58:56 +0200

    Add path info to scribtable object

--- Assets/Scripts/Player/GravityPath.cs
+++ Assets/Scripts/Player/GravityPath.cs
@@ -1,5 +1,6 @@
 using NordicGameJam.Player;
 using System.Collections.Generic;
+using NordicGameJam.SO;
 using UnityEngine;
 
 public class GravityPath : MonoBehaviour
@@ -8,6 +9,9 @@ public class GravityPath : MonoBehaviour
     public float PathSpeed;
     public BoxCollider2D Bounds;
     public Vector3 CurrentMomentum {get => PathMomentum;}
+
+    [SerializeField]
+    private PlayerInfo _info;
 
     private Transform PathVisual;
     public Vector3 PathMomentum { private set; get; }
@@ -31,7 +35,7 @@ public class GravityPath : MonoBehaviour
 
     void FixedUpdate()
     {
-        RenderPath(2);
+        RenderPath(_info.TimeAhead);
         if (_pc.DidMove)
         {
             (transform.position, PathMomentum) = PathDelta(transform.position, PathMomentum, Time.fixedDeltaTime * PathSpeed, GetAttractors());
@@ -54,7 +58,7 @@ public class GravityPath : MonoBehaviour
 
     private void RenderPath(float timeAhead)
     {
-        int pointRange = 100;
+        int pointRange = _info.SimPoints;
         float delta = timeAhead/pointRange;
 
         PathVisual.position = transform.position;
