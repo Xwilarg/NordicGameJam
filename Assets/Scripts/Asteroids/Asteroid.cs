18f53cbe2209c0ceeadb8fa08832358531622236
erik <enag2112@gmail.com>
2023-04-15 18:28:17 +0200

    Add asteroid homing

--- Assets/Scripts/Asteroids/Asteroid.cs
+++ Assets/Scripts/Asteroids/Asteroid.cs
@@ -1,42 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-
-    [SerializeField]
-    private GameObject _spawnTarget;
-
-
+    public Transform Target { set; private get; }
     public Rigidbody2D rb;
+    
+    private float _speed = 90.0f;
 
-    private float speed = 20.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
 
     private void FixedUpdate()
     {
-        if (_spawnTarget != null)
+        if (Target != null)
         {
-            Vector2 target = _spawnTarget.transform.position;
-            Vector2 direction = (target - rb.position).normalized;
-            Debug.Log("Direction");
-            Debug.Log(target);
-            Debug.Log(rb.position);
-            Debug.Log(direction);
-            rb.velocity = direction * speed * Time.deltaTime;
+            Vector2 direction = (Target.position - transform.position).normalized;
+            rb.velocity = direction * _speed * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Temple"))
+        {
+            Destroy(gameObject);
         }
     }
 }
