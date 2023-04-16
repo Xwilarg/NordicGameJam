3b3c9c238f96ff38bd14e66d6b00c94cec797403
Alexandre Coulon <alex97.coul@gmail.com>
2023-04-16 12:45:46 +0200

    Destroy asteroid VFX

--- Assets/Scripts/Asteroid/AsteroidController.cs
+++ Assets/Scripts/Asteroid/AsteroidController.cs
@@ -1,5 +1,6 @@
 using NordicGameJam.Coins;
 using UnityEngine;
+using System.Collections;
 
 namespace NordicGameJam.Asteroid
 {
@@ -8,21 +9,57 @@ namespace NordicGameJam.Asteroid
         [SerializeField]
         private GameObject[] _children;
 
+        [SerializeField]
+        private GameObject _destroyVFXContainer;
+
+        [SerializeField]
+        private float _dissolveSpeed = 2f;
+        private float _dissolveAmount = .8f;
+        private Material _dissolveMat;
+
         private void Awake()
         {
-            _children[Random.Range(0, _children.Length)].SetActive(true);
+            var go = _children[Random.Range(0, _children.Length)];
+            go.SetActive(true);
+            _dissolveMat = go.GetComponent<Renderer>().material;
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.collider.CompareTag("Temple"))
             {
-                Destroy(gameObject);
+                StartCoroutine(DissolveThenDestroy());
             }
             if (collision.collider.CompareTag("Player"))
             {
                 CoinManager.Instance.Spawn(transform.position, Temple.Instance.transform, false);
-                Destroy(gameObject);
+                StartCoroutine(DissolveThenDestroy());
+            }
+        }
+
+        private IEnumerator DissolveThenDestroy()
+        {
+            while (_dissolveAmount > 0)
+            {
+                _dissolveAmount -= Time.deltaTime * _dissolveSpeed;
+                _dissolveMat.SetFloat("_Dissolve", _dissolveAmount);
+                // delay the explosion VFX
+                if (!vfxPlayed && _dissolveAmount < 0.5f)
+                {
+                    PlayDestroyVFX();
+                    vfxPlayed = true;
+                }
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+        
+        private bool vfxPlayed = false;
+        private void PlayDestroyVFX()
+        {
+            foreach(ParticleSystem childPS in _destroyVFXContainer.GetComponentsInChildren<ParticleSystem>())
+            {
+                childPS.Play();
             }
         }
     }
